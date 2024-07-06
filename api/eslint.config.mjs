86467b9f6d24fc2db8476d0f67c65645fe61dbc3
Anonymous <anon@example.com>
2024-07-07 01:23:51 +0300

    add lint test

--- api/eslint.config.mjs
+++ api/eslint.config.mjs
@@ -1,12 +1,25 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+// eslint.config.mjs
+import pluginJs from '@eslint/js';
+import globals from 'globals';
 
-
-export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReactConfig,
+const config = [
+    {
+        languageOptions: {
+            globals: {
+                process: true,
+            },
+        },
+        plugins: {
+            '@eslint/js': pluginJs,
+        },
+        rules: {
+        },
+    },
+    {
+        files: ['**/*.js'],
+        rules: {
+        },
+    },
 ];
+
+export default config;
